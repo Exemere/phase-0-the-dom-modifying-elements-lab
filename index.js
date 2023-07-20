b19fb02d513ae5f08b16549ea6dbd6472a32e399
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-20 11:42:33 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+let newHeader = document.querySelector("h1#victory");
+
+newHeader.textContent = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion";
+
+
+const element = document.createElement("div");
 document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
